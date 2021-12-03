16fe5363bfa5e6b5bcacacd6b0bb4fc9878b9d61
calvin <cal.villa@icloud.com>
2021-12-02 21:41:43 -0500

    working catAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,38 @@
-var getAvailablePets = function() {
-    console.log("function was called");
-};
 
-getAvailablePets();
+function catFacts() {
+    fetch('https://cat-fact.herokuapp.com/facts')
+    .then(function(response) {
+      return response.json();
+    }).then(function(data) {
+       console.log('data', data);
+      var factsListEl = document.getElementById('cat-facts');
+  
+      for (var i = 0 ; i < data.length; i++) {
+            var factListItem = document.createElement('li');
+            factListItem.innerText = data[i].text;
+            factsListEl.appendChild(factListItem);
+      }
+    });
+  }
+  
+  catFacts(); 
 
 
+  function dogFacts() {
+    fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1')
+    .then(function(response) {
+      return response.json();
+    }).then(function(data) {
+       console.log('data', data);
+      var factsListEl = document.getElementById('dog-facts');
+  
+      for (var i = 0 ; i < data.length; i++) {
+            var factListItem = document.createElement('li');
+            factListItem.innerText = data[i].text;
+            factsListEl.appendChild(factListItem);
+      }
+    });
+  }
+  
+  dogFacts(); 
+  
