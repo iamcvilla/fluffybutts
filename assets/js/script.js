b331c23eb02fcdf1efab36a7f02089ae3264bfc8
Christina Crabtree <borrowedandblue@gmail.com>
2021-12-02 21:02:00 -0500

    adding auto access token

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,40 @@
 var accessToken = '';
-var tokenEl = document.getElementById('accesstoken');
-var buttonEl = document.getElementById('buttonToken');
-buttonEl.addEventListener('click', function() {
-   var apiKey = document.getElementById('apikey').value;
-   var apiSecret = document.getElementById('apisecret').value;
-  if (!apiKey) alert('You need to provide api key from your petfinder account settings page');
-  if (!apiSecret) alert('You need to provide api secret from your pet finder account settings page');
-  fetch('https://api.petfinder.com/v2/oauth2/token', {method: 'POST',                                                           headers: {
-      'Content-Type': 'application/json'
-    },
-body: JSON.stringify({grant_type: 'client_credentials',
-                      client_id:apiKey,
-                      client_secret: apiSecret})                                                                                                           }).then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log('data', data);
-    tokenEl.innerText = data.access_token;
-    accessToken = data.access_token;
-  }).catch(function(err) {
-    console.log('err', err);
-  });
-});
+function getAccessToken() {
+    var apiKey = "3Wnc44BRP16qJhv80lVv1yI8VZZLbe2B0udJQtJIw9UUBS3UI3";
+    var apiSecret = "NsSeJWrTc9EIPcPVtJORv2Mb0P5WYRBZDn89Pt7g";
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST', headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            grant_type: 'client_credentials',
+            client_id: apiKey,
+            client_secret: apiSecret
+        })
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log('data', data);
+        accessToken = data.access_token;
+        return accessToken;
+    }).then(function (accessToken) {
+        fetch('https://api.petfinder.com/v2/animals', {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }).then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            var animals = data.animals;
+            console.log('animals', animals);
+            for (var i = 0; i < animals.length; i++) {
+                console.log(animals[i].name + ': ' + animals[i].breeds.primary);
 
-var animalsButton = document.getElementById('getAnimals');
-var animalsListEl = document.getElementById('animals');
-animalsButton.addEventListener('click', function() {
-  if (accessToken) {
-      fetch('https://api.petfinder.com/v2/animals', {
-        headers: {
-          Authorization: 'Bearer ' + accessToken
-        }
-      }).then(function(response) {
-        return response.json()
-      }).then(function(data) {
-        var animals = data.animals;
-        loopAndRenderAnimalsOnPage(animals);
-      })
-  } else {
-    alert('Error: You need to get an oauth token first.');
-  }
+            }
+        }).catch(function (err) {
+            console.log('err', err);
+        })
+    });
+};
 
-});
-
-function loopAndRenderAnimalsOnPage(animals) {
-  console.log('animals', animals);
-  for (var i = 0; i < animals.length; i++) {
-    var listItemEl = document.createElement('li');
-    listItemEl.innerText = animals[i].name + ': ' + animals[i].breeds.primary;
-    animalsListEl.appendChild(listItemEl);
-    
-  }
-}
-
+getAccessToken();
