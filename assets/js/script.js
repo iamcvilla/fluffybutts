ad1d5e0cee8d6afa57d1a37fe48b308fe7fb1dfa
calvin <cal.villa@icloud.com>
2021-12-03 20:13:23 -0500

    hi team

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,24 +15,4 @@ function catFacts() {
     });
   }
   
-  catFacts(); 
-
-
-  function dogFacts() {
-    fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1')
-    .then(function(response) {
-      return response.json();
-    }).then(function(data) {
-       console.log('data', data);
-      var factsListEl = document.getElementById('dog-facts');
-  
-      for (var i = 0 ; i < data.length; i++) {
-            var factListItem = document.createElement('li');
-            factListItem.innerText = data[i].text;
-            factsListEl.appendChild(factListItem);
-      }
-    });
-  }
-  
-  dogFacts(); 
-  
+  catFacts();   
