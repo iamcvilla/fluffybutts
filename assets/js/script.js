6ee96cb1a76e793ea1aca4b2420b654a4174d5aa
Christina Crabtree <borrowedandblue@gmail.com>
2021-12-03 20:09:16 -0500

    fixing a booboo

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,19 @@ function getAccessToken() {
 };
 
 getAccessToken();
+
+function catFacts() {
+    fetch("https://cat-fact.herokuapp.com/facts")
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log("data", data);
+            var factsListEl = document.getElementById("cat-facts");
+            for (var i = 0; i < data.length; i++) {
+                var factListItem = document.createElement("li");
+                factListItem.innerText = data[i].text;
+                factsListEl.appendChild(factListItem);
+            }
+        });
+}
+catFacts();
