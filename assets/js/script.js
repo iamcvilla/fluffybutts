ffddf97856f3fc8337226387e8f90b29b6fc0c46
calvin <cal.villa@icloud.com>
2021-12-10 21:53:41 -0500

    making site responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,66 +2,66 @@ var myBtn = document.getElementById("myBtn");
 var savedpets =
       JSON.parse(window.localStorage.getItem("savedpets")) || [];
 var accessToken = '';
-function getAccessToken() {
-    var apiKey = "3Wnc44BRP16qJhv80lVv1yI8VZZLbe2B0udJQtJIw9UUBS3UI3";
-    var apiSecret = "NsSeJWrTc9EIPcPVtJORv2Mb0P5WYRBZDn89Pt7g";
-    fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST', headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            grant_type: 'client_credentials',
-            client_id: apiKey,
-            client_secret: apiSecret
-        })
-    }).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log('data', data);
-        accessToken = data.access_token;
-        return accessToken;
-    }).then(function (accessToken) {
-        fetch('https://api.petfinder.com/v2/animals?type=Cat&limit=12', {
-            headers: {
-                Authorization: 'Bearer ' + accessToken
-            }
-        }).then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            var petBox = document.querySelector(".pet-box")
-            var animals = data.animals;
-            console.log('animals', animals);
-            for (var i = 0; i < animals.length; i++) {
-                var petLi = document.createElement("li");
-                var viewButton = document.createElement("a")
-                viewButton.textContent = "view";
-                viewButton.classList.add ("btn", "btn-primary","btn-lg")
-                viewButton.classList.add ("btn", "btn-info","btn-lg", "btn-view")
-                viewButton.target = "_blank"
-                viewButton.href = animals[i].url
-                //viewButton.setAttribute("value", animals[i].url)
-                //viewButton.onclick = viewSelectedPet;
-                var saveButton = document.createElement("button")
-                saveButton.textContent = "save";
-                saveButton.classList.add ("btn", "btn-primary","btn-lg")
-                saveButton.classList.add ("btn", "btn-info","btn-lg", "btn-save")
-                saveButton.setAttribute("value", animals[i].id)
-                saveButton.setAttribute("petname",animals[i].name)
-                saveButton.onclick = saveSelectedPet;
-                petLi.textContent = animals[i].name + ': ' + animals[i].breeds.primary;
-                petLi.appendChild(viewButton)
-                petLi.appendChild(saveButton)
-               // let divEl = document.createElement("div");
-                //let pEl = document.createElement("p");
-                //pEl.textcontent =this.animals[i].description;
-                //divEl.append(pEl);
-                petBox.appendChild(petLi)
-            }
-        }).catch(function (err) {
-            console.log('err', err);
-        })
-    });
-};
+// function getAccessToken() {
+//     var apiKey = "3Wnc44BRP16qJhv80lVv1yI8VZZLbe2B0udJQtJIw9UUBS3UI3";
+//     var apiSecret = "NsSeJWrTc9EIPcPVtJORv2Mb0P5WYRBZDn89Pt7g";
+//     fetch('https://api.petfinder.com/v2/oauth2/token', {
+//         method: 'POST', headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             grant_type: 'client_credentials',
+//             client_id: apiKey,
+//             client_secret: apiSecret
+//         })
+//     }).then(function (response) {
+//         return response.json();
+//     }).then(function (data) {
+//         console.log('data', data);
+//         accessToken = data.access_token;
+//         return accessToken;
+//     }).then(function (accessToken) {
+//         fetch('https://api.petfinder.com/v2/animals?type=Cat&limit=12', {
+//             headers: {
+//                 Authorization: 'Bearer ' + accessToken
+//             }
+//         }).then(function (response) {
+//             return response.json()
+//         }).then(function (data) {
+//             var petBox = document.querySelector(".pet-box")
+//             var animals = data.animals;
+//             console.log('animals', animals);
+//             for (var i = 0; i < animals.length; i++) {
+//                 var petLi = document.createElement("li");
+//                 var viewButton = document.createElement("a")
+//                 viewButton.textContent = "view";
+//                 viewButton.classList.add ("btn", "btn-primary","btn-lg")
+//                 viewButton.classList.add ("btn", "btn-info","btn-lg", "btn-view")
+//                 viewButton.target = "_blank"
+//                 viewButton.href = animals[i].url
+//                 //viewButton.setAttribute("value", animals[i].url)
+//                 //viewButton.onclick = viewSelectedPet;
+//                 var saveButton = document.createElement("button")
+//                 saveButton.textContent = "save";
+//                 saveButton.classList.add ("btn", "btn-primary","btn-lg")
+//                 saveButton.classList.add ("btn", "btn-info","btn-lg", "btn-save")
+//                 saveButton.setAttribute("value", animals[i].id)
+//                 saveButton.setAttribute("petname",animals[i].name)
+//                 saveButton.onclick = saveSelectedPet;
+//                 petLi.textContent = animals[i].name + ': ' + animals[i].breeds.primary;
+//                 petLi.appendChild(viewButton)
+//                 petLi.appendChild(saveButton)
+//                // let divEl = document.createElement("div");
+//                 //let pEl = document.createElement("p");
+//                 //pEl.textcontent =this.animals[i].description;
+//                 //divEl.append(pEl);
+//                 petBox.appendChild(petLi)
+//             }
+//         }).catch(function (err) {
+//             console.log('err', err);
+//         })
+//     });
+// };
 function viewSelectedPet(){
     var petID = this.value
     console.log(petID)
