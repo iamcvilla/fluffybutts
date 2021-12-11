0629dc3332c3e9a470ebd1d1df931fd7a1dd7fa3
Christina Crabtree <borrowedandblue@gmail.com>
2021-12-11 17:30:12 -0500

    adding search form elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,76 @@
+
+var searchFormEl = document.getElementById("search-form");
+console.log("searchFormEl", searchFormEl)
+var postCodeEl = document.getElementById("postcode");
+console.log("postCodeEl", postCodeEl)
+var searchBtn = document.getElementById("search-pet-btn");
+var postcode = "";
 var myBtn = document.getElementById("myBtn");
 var savedpets =
       JSON.parse(window.localStorage.getItem("savedpets")) || [];
 var accessToken = '';
-// function getAccessToken() {
-//     var apiKey = "3Wnc44BRP16qJhv80lVv1yI8VZZLbe2B0udJQtJIw9UUBS3UI3";
-//     var apiSecret = "NsSeJWrTc9EIPcPVtJORv2Mb0P5WYRBZDn89Pt7g";
-//     fetch('https://api.petfinder.com/v2/oauth2/token', {
-//         method: 'POST', headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             grant_type: 'client_credentials',
-//             client_id: apiKey,
-//             client_secret: apiSecret
-//         })
-//     }).then(function (response) {
-//         return response.json();
-//     }).then(function (data) {
-//         console.log('data', data);
-//         accessToken = data.access_token;
-//         return accessToken;
-//     }).then(function (accessToken) {
-//         fetch('https://api.petfinder.com/v2/animals?type=Cat&limit=12', {
-//             headers: {
-//                 Authorization: 'Bearer ' + accessToken
-//             }
-//         }).then(function (response) {
-//             return response.json()
-//         }).then(function (data) {
-//             var petBox = document.querySelector(".pet-box")
-//             var animals = data.animals;
-//             console.log('animals', animals);
-//             for (var i = 0; i < animals.length; i++) {
-//                 var petLi = document.createElement("li");
-//                 var viewButton = document.createElement("a")
-//                 viewButton.textContent = "view";
-//                 viewButton.classList.add ("btn", "btn-primary","btn-lg")
-//                 viewButton.classList.add ("btn", "btn-info","btn-lg", "btn-view")
-//                 viewButton.target = "_blank"
-//                 viewButton.href = animals[i].url
-//                 //viewButton.setAttribute("value", animals[i].url)
-//                 //viewButton.onclick = viewSelectedPet;
-//                 var saveButton = document.createElement("button")
-//                 saveButton.textContent = "save";
-//                 saveButton.classList.add ("btn", "btn-primary","btn-lg")
-//                 saveButton.classList.add ("btn", "btn-info","btn-lg", "btn-save")
-//                 saveButton.setAttribute("value", animals[i].id)
-//                 saveButton.setAttribute("petname",animals[i].name)
-//                 saveButton.onclick = saveSelectedPet;
-//                 petLi.textContent = animals[i].name + ': ' + animals[i].breeds.primary;
-//                 petLi.appendChild(viewButton)
-//                 petLi.appendChild(saveButton)
-//                // let divEl = document.createElement("div");
-//                 //let pEl = document.createElement("p");
-//                 //pEl.textcontent =this.animals[i].description;
-//                 //divEl.append(pEl);
-//                 petBox.appendChild(petLi)
-//             }
-//         }).catch(function (err) {
-//             console.log('err', err);
-//         })
-//     });
-// };
+function getAccessToken() {
+    var apiKey = "3Wnc44BRP16qJhv80lVv1yI8VZZLbe2B0udJQtJIw9UUBS3UI3";
+    var apiSecret = "NsSeJWrTc9EIPcPVtJORv2Mb0P5WYRBZDn89Pt7g";
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST', headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            grant_type: 'client_credentials',
+            client_id: apiKey,
+            client_secret: apiSecret
+        })
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log('data', data);
+        accessToken = data.access_token;
+        return accessToken;
+    }).then(function (accessToken) {
+        fetch('https://api.petfinder.com/v2/animals?type=Cat&limit=12', {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }).then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            var petBox = document.querySelector(".pet-box")
+            console.log("data", data)
+            var animals = data.animals;
+        
+            for (var i = 0; i < animals.length; i++) {
+                var petLi = document.createElement("li");
+                var viewButton = document.createElement("a")
+                viewButton.textContent = "view";
+                viewButton.classList.add ("btn", "btn-primary","btn-lg")
+                viewButton.classList.add ("btn", "btn-info","btn-lg", "btn-view")
+                viewButton.target = "_blank"
+                viewButton.href = animals[i].url
+                viewButton.setAttribute("value", animals[i].url)
+                viewButton.onclick = viewSelectedPet;
+                var saveButton = document.createElement("button")
+                saveButton.textContent = "save";
+                saveButton.classList.add ("btn", "btn-primary","btn-lg")
+                saveButton.classList.add ("btn", "btn-info","btn-lg", "btn-save")
+                saveButton.setAttribute("value", animals[i].id)
+                saveButton.setAttribute("petname",animals[i].name)
+                saveButton.onclick = saveSelectedPet;
+                petLi.textContent = animals[i].name + ': ' + animals[i].breeds.primary;
+                petLi.appendChild(viewButton)
+                petLi.appendChild(saveButton)
+                let divEl = document.createElement("div");
+                let pEl = document.createElement("p");
+                console.log("animals.description", animals[i].description)
+                pEl.textcontent = animals[i].description;
+                divEl.append(pEl);
+                petBox.appendChild(petLi)
+            }
+        }).catch(function (err) {
+            console.log('err', err);
+        })
+    });
+};
 function viewSelectedPet(){
     var petID = this.value
     console.log(petID)
@@ -73,15 +82,29 @@ function saveSelectedPet(){
     savedpets =
       JSON.parse(window.localStorage.getItem("savedpets")) || [];
     var newPet = {
-       // name: this.petname,
+       name: this.petname,
         id: petID
     }
-    //console.log(this.petname);
+    console.log(this.petname);
     savedpets.push (newPet);
     window.localStorage.setItem("savedpets", JSON.stringify(savedpets));
     $('#myModal_product').modal('show');
     console.log(petID)
 }
+
+var catSearchHandler = function(event) {
+    event.preventDefault();
+    
+    // search and manipulate dom off search
+    var searchedPostcodeEl = document.getElementById("postcode");
+    var searchedPostcode = searchedPostcodeEl.value.trim();
+    postcode = searchedPostcode;
+    
+    console.log("event", event);
+
+    
+};
+     
 getAccessToken();
 function catFacts() {
     var factsListEl = document.getElementById("cat-facts");
@@ -91,12 +114,13 @@ function catFacts() {
             return response.json();
         }).then(function (data) {
             console.log("data", data);
-            // for (var i = 0; i < data.length; i++) {
-            // }
+            for (var i = 0; i < data.length; i++) {
+             }
             var index = Math.floor(Math.random() * data.length)
             var factListItem = document.createElement("p");
             factListItem.innerText = data[index].text;
             factsListEl.appendChild(factListItem);
         });
 }
+searchFormEl.addEventListener("submit", catSearchHandler);
 myBtn.addEventListener("click", catFacts)
